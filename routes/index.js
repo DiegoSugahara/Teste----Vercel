aea84eb099ead11be3699fa37b31b3a116dc027a
Diego Sugahara <diyurisimao@gmail.com>
2024-11-04 11:50:44 -0300

    Att

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-//listar
-router.get('/', async(req, res) => {
-    res.render("base", {
-        title: "Home",
-        view: "index"
-    });
+//mostrar a index.ejs
+router.get("/", (req, res) => {
+  res.render("base", {
+    title: "Home",
+    view: "index"
+  });
 });
-
 module.exports = router;
